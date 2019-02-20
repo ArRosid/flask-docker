18351f1824d376bee5b26992b345c52fc7a2f211
Ahmad Rosid Komarudin <ahmadrosid30121997@gmail.com>
2019-02-20 20:59:57 +0700

    styling using bootstrap

--- web_app/app.py
+++ web_app/app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
+    app.config['DEBUG'] = True
 
     @app.route('/')
     def index():
-        return 'Hello World'
+        return render_template('index.html', title='Flask 01 - Home')
 
     @app.route('/about')
     def about():
-        return 'About Me'
+        return render_template('about.html', title='Flask 01 - About')
 
     return app
